9ea2d8b80b8cf5de605d26c07e5610b2c763cee7
Keenan McKenzie <keenan@mckenziemail.com>
2020-05-02 18:17:29 -0500

    update cors for s3 react frontend

--- src/main/java/com/mckenzie/fridge/controller/ItemController.java
+++ src/main/java/com/mckenzie/fridge/controller/ItemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/api")
 public class ItemController {
