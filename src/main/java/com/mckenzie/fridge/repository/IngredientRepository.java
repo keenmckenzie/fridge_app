4d964d1461575efbbbdca80c11afcf7b106d64e5
Keenan McKenzie <keenan@mckenziemail.com>
2020-05-06 12:29:22 -0500

    update findByName method

--- src/main/java/com/mckenzie/fridge/repository/IngredientRepository.java
+++ src/main/java/com/mckenzie/fridge/repository/IngredientRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    Optional<Ingredient> findByIngredientName(String name);
+    Optional<Ingredient> findByName(String name);
 
 }
