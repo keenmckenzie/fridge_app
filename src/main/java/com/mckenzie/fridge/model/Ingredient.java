e93d30b01c2e5d23ccb8592f2573cf9e699ddd78
Keenan McKenzie <keenan@mckenziemail.com>
2020-05-08 16:58:19 -0500

    remove ingredient constructor

--- src/main/java/com/mckenzie/fridge/model/Ingredient.java
+++ src/main/java/com/mckenzie/fridge/model/Ingredient.java
@@ -19,11 +19,6 @@ public class Ingredient {
 
     private int goodFor;
 
-    public Ingredient(Long id, @NotBlank String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     public Long getId() {
         return id;
     }
